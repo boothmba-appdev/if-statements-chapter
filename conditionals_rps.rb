8b67f5d27eafc60e3ef88d8a6eb98b52f09591c4
hxie0 <xhy@chicagobooth.edu>
2021-07-07 06:05:55 +0000

    if statement completed!

--- conditionals_rps.rb
+++ conditionals_rps.rb
@@ -7,3 +7,17 @@
 # If you need a refresher on the rules of Rock, Paper, Scissors: https://en.wikipedia.org/wiki/Rock%E2%80%93paper%E2%80%93scissors
 
 p "Please choose rock, paper, or scissors:"
+choice = gets.chomp
+if choice.include?("rock")
+    p "You played rock!"
+    p "The computer played scissors!"
+    p "You won!"
+elsif choice.include?("paper")
+    p "You played paper!"
+    p "The computer played scissors!"
+    p "You lost!"
+else 
+    p "You played scissors!"
+    p "The computer played scissors!"
+    p "You tied!"
+end
